e2d9ee1bc90d530380d150378be624b9324f4a97
lucien1011 <lucien1011@gmail.com>
2018-04-16 10:54:44 -0400

    update skim cfg

--- skim_SingleMuon2017_cfg.py
+++ skim_SingleMuon2017_cfg.py
@@ -1,7 +1,7 @@
 from Dataset.SingleMuon2017 import *
 
 componentList       = comp_SingleMuon2017E_v1_partial
-nCores              = 6
+nCores              = 8
 disableProgressBar  = False
-outputPathTemplate = "/raid/raid7/lucien/CSC/GasGain/Log/2018-04-08/SingleMuon2017E_v1_partial/SkimTree_%s.root"
+outputPathTemplate = "/raid/raid7/lucien/CSC/GasGain/Log/2018-04-15/SingleMuon2017E_v1_partial/SkimTree_%s.root"
 
