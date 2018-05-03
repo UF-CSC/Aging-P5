a2c6e9a0e38d779e5db42942f03cf6ec801177a4
lucien1011 <lucien1011@gmail.com>
2018-05-03 12:08:20 -0400

    update config

--- run_SkimTree-SingleMuon2017_cfg.py
+++ run_SkimTree-SingleMuon2017_cfg.py
@@ -4,7 +4,7 @@ from Core.EndSequence import EndSequence
 from Core.OutputInfo import OutputInfo 
 
 from Dataset.Utils import makeComponentsFromPath
-from Core.NanoAODResult.Component import Component
+from Core.Component import Component
 
 from Skimmer.ZMuMuSkimmer import SkimTreeZMuMuSkimmer
 from Analyzer.GasGainAnalyzer import SkimTreeGasGainPlotter,SkimTreePositionPlotter
@@ -14,14 +14,15 @@ from EndModule.GasGainEndModule import SkimTreeGasGainEndModule
 paths_SingleMuon2017E_v1_skim = ["/raid/raid7/lucien/CSC/GasGain/Log/2018-03-19/SingleMuon2017E_v1_partial_hadd/"]
 paths_SingleMuon2017E_v2_skim = ["/raid/raid7/lucien/CSC/GasGain/Log/2018-04-08/SingleMuon2017E_v2_partial_hadd/"]
 paths_SingleMuon2017E_v3_skim = ["/raid/raid7/lucien/CSC/GasGain/Log/2018-04-15/SingleMuon2017E_v3_partial_hadd/"]
+paths_SingleMuon2017E_v4_skim = ["/raid/raid7/lucien/CSC/GasGain/Log/2018-04-15/SingleMuon2017E_v4_partial_hadd/"]
 
 x_segments = [-150+i*15 for i in range(0,21)]
 y_segments = [-200+i*20 for i in range(0,21)]
 
-paths = paths_SingleMuon2017E_v3_skim
+paths = paths_SingleMuon2017E_v4_skim
 componentList = []
 for path in paths:
-    tmp = Component(path+"/",path.split("/")[-1],keyword="",inUFTier2=False)
+    tmp = Component(path+"/",path.split("/")[-1],keyword="ME11",inUFTier2=False)
     componentList.extend(tmp.makeComponentFromEachFile())
 
 rootTree            = "tree"
@@ -33,8 +34,8 @@ nEvents             = -1
 #outputDir           = "/raid/raid7/lucien/CSC/GasGain/Log/2018-03-19/comp_SingleMuon2017E_v1_skim_anl_all_ZMuMu_allChannel/"
 
 #outputDir           = "/raid/raid7/lucien/CSC/GasGain/Log/2018-04-08/comp_SingleMuon2017E_v2_skim_anl_all_ZMuMu_allChannel/"
-outputDir           = "/raid/raid7/lucien/CSC/GasGain/Log/2018-04-16/comp_SingleMuon2017E_v3_skim_anl_all_ZMuMu_allChannel_4/"
-justEndSequence     = True
+outputDir           = "/raid/raid7/lucien/CSC/GasGain/Log/2018-05-03/comp_SingleMuon2017E_v3_skim_anl_all_ZMuMu_allChannel_CombinedME11ab/"
+justEndSequence     = True 
 
 sequence = Sequence()
 sequence.add(SkimTreeZMuMuSkimmer("ZMuMuSkimmer"))
@@ -49,7 +50,7 @@ endSequence = EndSequence()
 #endModuleOutputDir = "/home/lucien/public_html/CSC/GasGainAnalysis/Log/2018-03-21/InclusiveSelection/"
 #endModuleOutputDir = "/home/lucien/public_html/CSC/GasGainAnalysis/Log/2018-03-21/ZMuMuSelection/"
 #endModuleOutputDir = "/home/lucien/public_html/CSC/GasGainAnalysis/Log/2018-04-16/ZMuMuSelection_ExcludeBadChannels/"
-endModuleOutputDir = "/home/lucien/public_html/CSC/GasGainAnalysis/Log/2018-04-16/ZMuMuSelection_ExcludeBadChannels_PosStep20/"
+endModuleOutputDir = "/home/lucien/public_html/CSC/GasGainAnalysis/Log/2018-05-03/ZMuMuSelection_ExcludeBadChannels_PosStep20_CombinedME11ab/"
 skimTreeGasGainEndModule = SkimTreeGasGainEndModule(endModuleOutputDir)
 endSequence.add(skimTreeGasGainEndModule)
 
--- skim_SingleMuon2017_cfg.py
+++ skim_SingleMuon2017_cfg.py
@@ -1,7 +1,7 @@
 from Dataset.SingleMuon2017 import *
 
 componentList       = comp_SingleMuon2017_partial
-nCores              = 1
+nCores              = 8
 disableProgressBar  = False
 outputPathTemplate = "/raid/raid7/lucien/CSC/GasGain/Log/2018-05-03/comp_SingleMuon2017CH_partial/SkimTree_%s.root"
 
